03e877ec1a0d7cc6687a4f9faa7acef60820db43
Jorge Martín Martí <tcbpepo@gmail.com>
2015-07-09 17:55:22 +0200

    Karma & Bower config files

--- karma.conf.js
+++ karma.conf.js
@@ -4,16 +4,17 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-loader/angular-loader.js',
-      'app/bower_components/jquery/dist/jquery.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js',
-      'app/modules/**/*.test.js'
+      {pattern: 'app/bower_components/angular/angular.js', included: true},
+      {pattern: 'app/bower_components/angular-loader/angular-loader.js', included: true},
+      {pattern: 'app/bower_components/angular-scenario/angular-scenario.js', included: false},
+      {pattern: 'app/bower_components/angular-mocks/angular-mocks.js', included: true},
+      {pattern: 'app/bower_components/angular-resource/angular-resource.js', included: true},
+      {pattern: 'app/bower_components/angular-route/angular-route.js', included: true},
+      {pattern: 'app/bower_components/jquery/dist/jquery.js', included: true},
+      //{pattern: 'app/bower_components/protractor/lib/runner.js', included: false},
+      'app/loaders/**/*.test.js'
     ],
+
 
     autoWatch : true,
 
@@ -22,22 +23,30 @@ module.exports = function(config){
     },
 
     frameworks: [
+              //'protactor',
               //'ng-scenario',
+              //'mocha'
               'jasmine'
               ],
 
-    browsers : ['Chrome'],
+    browsers : ['Chrome'
+              //, 'Firefox'
+              ],
 
     plugins : [
             //'karma-ng-scenario',
             'karma-chrome-launcher',
+            'karma-firefox-launcher',
             'karma-jasmine',
+            //'karma-mocha',
+            'protractor',
             'karma-junit-reporter'
             ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
-    }
+    },
+    //exclude: ['app/bower_components/angular-mocks/ngAnimateMock.js']
   });
 };

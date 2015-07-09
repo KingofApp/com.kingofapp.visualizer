da60e0e9d263004ba9f82058596d05ed34d26225
Jorge Martín Martí <tcbpepo@gmail.com>
2015-07-09 17:50:29 +0200

    loaderAngular e2e test file

--- app/loaders/angular/loaderAngular.e2e.test.js
+++ app/loaders/angular/loaderAngular.e2e.test.js
@@ -1,20 +1,21 @@
 (function(){
 	'use strict';
-	console.log("BeforeDescribe");
 	describe('loaderAngular', function() {
-		console.log("InDescribe");
+
 		beforeEach(module('king.loaders.common'));
-		console.log("After-beforeEach");
+
 	    it('should load a Angular module on the first call', function() {
-	    	console.log("Inside IT");
-	    	browser.get('http://localhost:8000/app/#/ang-module');
-	    	expect(element(by.tagName('p')).getText()).toBe('URLqwdqw');
-    		console.log(element(by.tagName('p')).getText());
+			/*
+			beforeEach(function(){
+			});
+			*/
+			console.log("Entra aqui");
+			browser.get('/app/#/angmodule');
+
+	    	//expect(element(by.tagName('p')).getText()).toBe('URLqwdqw');
+    		//console.log(element(by.tagName('p')).getText());
 	    });
-	    console.log("LAST");
+
 	});
 
 }());
-
-
-

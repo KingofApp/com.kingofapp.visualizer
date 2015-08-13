85c144c97c846c48b85e53588da7368ccc8f24c9
José Vicente Sogorb <joviwap@gmail.com>
2015-08-13 13:40:25 +0200

    Removing references to phantomjs on protractor test

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -15,13 +15,13 @@ exports.config = {
      * Can be used to specify the phantomjs binary path.
      * This can generally be ommitted if you installed phantomjs globally.
      */
-    'phantomjs.binary.path': require('phantomjs').path,
+    //'phantomjs.binary.path': require('phantomjs').path,
 
     /*
      * Command line args to pass to ghostdriver, phantomjs's browser driver.
      * See https://github.com/detro/ghostdriver#faq
      */
-    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+    //'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
   },
 
   baseUrl: 'http://localhost:8000/app/',

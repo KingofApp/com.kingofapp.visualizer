634e5d55f92bc1fd8885b0b27dba48fc1904f582
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2015-10-15 17:34:14 +0200

    Puship notifications

--- app/loaders/common/loaderCommon.controller.js
+++ app/loaders/common/loaderCommon.controller.js
@@ -32,27 +32,6 @@
 
   function commonLoaderCtrl($scope, $rootScope, $location, $ocLazyLoad, structureService, angularLoader, trafficGuardiaCivil) {
     console.log('pasa por el commonLoaderCtrl');
-    // API : AIzaSyBQ3w6f5mFXyOK5sWC2oXzYcjLbN8IHRjg
-    // SenderId: 341724041512
-        // var push = PushNotification.init({ "android": {"senderID": "12345679"},
-        //      "ios": {"alert": "true", "badge": "true", "sound": "true"}, "windows": {} } );
-        //
-        // push.on('registration', function(data) {
-        //     // data.registrationId
-        // });
-        //
-        // push.on('notification', function(data) {
-        //     // data.message,
-        //     // data.title,
-        //     // data.count,
-        //     // data.sound,
-        //     // data.image,
-        //     // data.additionalData
-        // });
-        //
-        // push.on('error', function(e) {
-        //     // e.message
-        // });
 
     $scope.trafficGuardiaCivil = trafficGuardiaCivil;
     var prev = 0;

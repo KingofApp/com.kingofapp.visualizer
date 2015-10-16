c05e7dc90fffa3576439ecea96686d9fc7fccb12
pepocivs <tcbpepo@gmail.com>
2015-10-16 16:45:18 +0200

    Fix menu load & add promise to setMenu

--- app/modules/polymermenu/controller.js
+++ app/modules/polymermenu/controller.js
@@ -1,26 +1,42 @@
+'use strict';
 angular
   .controller('polymerMenuCtrl', loadFunction);
 
-loadFunction.$inject = ['$scope', 'structureService', '$location'];
+loadFunction.$inject = ['$q', '$scope', 'structureService', '$location'];
 
-function loadFunction($scope, structureService, $location) {
+function loadFunction($q, $scope, structureService, $location) {
   //Register upper level modules
   structureService.registerModule($location, $scope, 'polymermenu');
 
   // TODO: Simplifie polymer data loading
-  var menu = new Array(0);
 
-  angular.forEach(structureService.getChildren($scope.polymermenu.modulescope.path), function(value, key) {
-    structureService.getModule(key).then(function(module) {
-      if (!module.hidden) {
-        menu.push({
-          text: value.name,
-          url: '#' + key,
-        });
-      }
-    });
 
+  $q.all({
+        menu: getMenu()
+      })
+      .then(function(data){
+        setMenu(data.menu);
   });
 
-  $scope.polymermenu.menu = menu;
+
+
+  function getMenu(){
+    var menu = new Array(0);
+      angular.forEach(structureService.getChildren($scope.polymermenu.modulescope.path), function(value, key) {
+        structureService.getModule(key).then(function(module) {
+          if (!module.hidden) {
+            menu.push({
+              text: value.name,
+              url: '#' + key,
+            });
+          }
+        });
+    });
+    return menu;
+  }
+
+  function setMenu(menu){
+    $scope.polymermenu.menu = menu;
+  }
+
 }

b06c727f9c23db7da3ea6a4785aef2cfb92db7d5
Abdón Rodríguez Davila <thepeer@kingofapp.es>
2016-01-13 11:49:10 +0100

    Clean loaderCommon

--- app/loaders/common/loaderCommon.controller.js
+++ app/loaders/common/loaderCommon.controller.js
@@ -9,8 +9,11 @@
 
   function commonLoaderCtrl($scope, $window, $rootScope, $route, $location, structureService, angularLoader, trafficGuardiaCivil) {
     console.log('Pasa por el commonLoaderCtrl');
+
     var koaApp = document.querySelector('#koaApp');
+
     $rootScope.showTransition = true;
+
     if (structureService.getIndex() !== '' && ($location.$$path === '/' || $location.$$path === '')) {
       $location.path(structureService.getIndex());
     }
@@ -29,13 +32,11 @@
       },
 
       function handleModelChange(count) {
-        console.log(
-          'Pending HTTP count:', count,
-          '{',
+        console.log('Pending HTTP count:', count,
+        '{',
           trafficGuardiaCivil.pending.get, 'GET ,',
           trafficGuardiaCivil.pending.post, 'POST',
-          '}'
-        );
+        '}');
 
         if ($location.$$path !== '/') {
           setTimeout(function() {
@@ -66,16 +67,17 @@
       if (structureService.get() !== newValue && newValue !== undefined && !redirected) {
         structureService.set(newValue);
         redirected = true;
+
         setTimeout(function() {
           $scope.$apply(function() {
             //Causing first load to not render KOA
             setTheme(newValue.themes.android.name);
+
             if (newValue.config.index === $location.path()) {
               $route.reload();
             } else {
               $location.path(newValue.config.index);
             }
-
           });
         }, 100);
       }
@@ -91,6 +93,7 @@
     $scope.$watch('appModules', function(newValue, oldValue) {
       if (oldValue !== newValue && newValue) {
         structureService.setModules(newValue.modules);
+
         if (newValue.index === $location.path()) {
           $route.reload();
         } else {
@@ -102,7 +105,9 @@
     $scope.$watch('appTheme', function(newValue, oldValue) {
       if (oldValue !== newValue) {
         console.log('Theme', newValue);
+
         setTheme(newValue);
+
         setTimeout(function() {
           $scope.$apply(function() {
             $route.reload();
@@ -119,39 +124,40 @@
 
     $scope.$on('koaLaunched', function(event, args) {
       console.log('Koa Launched');
+
       $rootScope.$apply(function() {
-        // TODO - Fixer to force android render
-        document.body.scrollTop = 1;
-        document.body.scrollTop = 0;
         $rootScope.showTransition = false;
       });
     });
-    //TODO: INSPECT loadconfig
-    //Load config
+
+    // TODO: INSPECT loadconfig
+    // Load config
     structureService.loadconfig($rootScope);
 
-    //Register Route
+    // Register Route
     structureService.getModule($location.$$path).then(function(module) {
       $rootScope.toolbar = {
         title: module.name
       };
+
       if ($rootScope.missing) {
         $rootScope.showTransition = false;
       }
+
       $rootScope.current = module.identifier;
       $scope.module = module || $scope.module;
 
       if (!module.type) {
-        //Display a 404 error or similar structureService.getIndex() === '' &&
+        // Display a 404 error or similar structureService.getIndex() === '' &&
         if ($location.$$path !== '/') {
           $location.path('/404');
         }
       } else if (isAngularModule(module.type)) {
         angularLoader.module($scope);
       } else if (isJqueryModule(module.type)) {
-        //TODO: Load jquery module from angular
+        // TODO: Load jquery module from angular
       } else {
-        //TODO: Display error and blame developer
+        // TODO: Display error and blame developer
       }
     });
 
@@ -171,15 +177,16 @@
     }
 
     function addEvents() {
+      // Get current scope
       var $scope = angular.element(document.querySelector('.' + $rootScope.current)).scope();
 
-      // console.info('Adding ng-click...');
+      // Adding ng-click...
       $('[ng-click]').click(function() {
         var functionName = $(this).attr('ng-click').replace('()', '');
         $scope[functionName]();
       });
 
-      // console.info('Adding ng-model...');
+      // Adding ng-model...
       $('[ng-model]').each(function() {
         var parent = $(this);
 
@@ -199,9 +206,7 @@
 
     function launchKoa() {
       setTimeout(function() {
-
-        // console.info('Changing koa-elements to theme-elements...');
-
+        // Changing koa-elements to theme-elements...
         koaApp.createTree();
 
         if (!koaApp.theme) {
@@ -213,7 +218,6 @@
         } else {
           koaApp.renderThemeElements(addEvents);
         }
-
       }, 100);
     }
   }

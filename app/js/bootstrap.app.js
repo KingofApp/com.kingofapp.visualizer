f2fd469f27558e24654ba4102957d165337761ee
Abdón Rodríguez Davila <thepeer@kingofapp.es>
2016-01-21 10:56:44 +0100

    Beauty bootstrap.app.js

--- app/js/bootstrap.app.js
+++ app/js/bootstrap.app.js
@@ -1,24 +1,22 @@
-angular.element(document).ready(function () {
-    $.getJSON('core/structure.json', function (data) {
+angular.element(document).ready(function() {
+  $.getJSON('core/structure.json', function(data) {
+    angular
+      .module('myApp').run(function run($rootScope) {
+        $rootScope.appJsonStructure = data;
+      })
+      .config(['configServiceProvider', function(configServiceProvider) {
+        configServiceProvider.config({
+          services: data.services
+        });
+      }]);
 
-        angular
-          .module('myApp').run( function run($rootScope) {
-              $rootScope.appJsonStructure = data;
-          })
-          .config(['configServiceProvider', function (configServiceProvider) {
-            configServiceProvider.config({
-                services: data.services
-            });
-        }]);
-
-
-        angular.bootstrap(document, ['myApp']);
-        console.log("Bootstrapd  ng-app");
-    }).fail(function() {
-      console.log("Default loading from samplemodules");
-      angular.module('myApp').constant( 'redirectUrl', '' );
-      angular.bootstrap(document, ['myApp']);
-      console.log("Bootstrapd  ng-app");
-      window.parent.postMessage('bootstrapped-app','*');
-    });
+    angular.bootstrap(document, ['myApp']);
+    console.log('Bootstraped ng-app');
+  }).fail(function() {
+    console.log('Default loading from samplemodules');
+    angular.module('myApp').constant('redirectUrl', '');
+    angular.bootstrap(document, ['myApp']);
+    console.log('Bootstraped ng-app');
+    window.parent.postMessage('bootstrapped-app', '*');
+  });
 });

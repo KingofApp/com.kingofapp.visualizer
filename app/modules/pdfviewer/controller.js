9521e6cb65eca3c80e798ea78c6e094c91b708d5
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-01-26 18:40:33 +0100

    pdfviewer ready to publish

--- app/modules/pdfviewer/controller.js
+++ app/modules/pdfviewer/controller.js
@@ -7,24 +7,4 @@ function loadFunction($scope, structureService, $location){
   //Register upper level modules
   structureService.registerModule($location,$scope,"pdfviewer");
 
-  $scope.pdfUrl = $scope.pdfviewer.modulescope.value;
-  $scope.scroll = 0;
-  $scope.loading = 'loading';
-  $scope.ready='';
-
-  $scope.getNavStyle = function(scroll) {
-    if(scroll > 100) return 'pdf-controls fixed';
-    else return 'pdf-controls';
-  }
-
-  $scope.onError = function(error) {
-    console.log(error);
-  }
-
-  $scope.onLoad = function() {
-    $scope.loading = '';
-    if($scope.pdfviewer.modulescope.debug){
-      $scope.ready = '<p class="ready">Ready!</p>';
-    }
-  }
 }

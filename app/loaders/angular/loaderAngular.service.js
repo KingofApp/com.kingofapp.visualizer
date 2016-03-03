b1e63fa42acfe6d7b28e2f83662f2489036366b4
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-03-03 18:19:08 +0100

    PolymerImportHref function restored.

--- app/loaders/angular/loaderAngular.service.js
+++ app/loaders/angular/loaderAngular.service.js
@@ -78,9 +78,29 @@
 
         function importHref(file) {
           var defer = $q.defer();
-          Polymer.Base.importHref(file, defer.resolve, defer.reject);
+          polymerImportHref(file, defer.resolve, defer.reject);
           return defer.promise;
         }
+
+        function polymerImportHref(href, onload, onerror) {
+          var l = document.createElement('link');
+          l.rel = 'import';
+          l.href = href;
+          var self = this;
+          if (onload) {
+            l.onload = function(e) {
+              return onload.call(self, e);
+            };
+          }
+          if (onerror) {
+            l.onerror = function(e) {
+              return onerror.call(self, e);
+            };
+          }
+          document.head.appendChild(l);
+          return l;
+        }
+
         function filterHtml(n) {
           return n != undefined && n.indexOf('.html') > -1
         }

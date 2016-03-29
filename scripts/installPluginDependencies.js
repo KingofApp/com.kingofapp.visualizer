f296cfbdfdbd0ec9434c052b5f400d4803068ebb
José Vicente Sogorb Morón <joviwap@gmail.com>
2016-03-29 18:25:57 +0200

    added script to install plugin dependencies

---
+++ scripts/installPluginDependencies.js
@@ -0,0 +1,35 @@
+var sh    = require('shelljs');
+var async = require('async');
+
+var local = sh.pwd();
+
+async.forEachSeries(sh.ls('app/modules'), downloadPlugin('modules') , function(err, data){
+  sh.cd(local);
+  async.forEachSeries(sh.ls('app/themes'),  downloadPlugin('themes') );
+});
+
+function downloadPlugin(type){
+  return function(plugin, callback){
+    if(plugin){
+      console.log('*** Installing dependencies from: app/modules/'+plugin);
+      async.series([
+        async.asyncify( async.apply(sh.cd, local) ),
+        async.asyncify( async.apply(sh.cd, 'app/'+type+'/'+plugin) ),
+        async.apply(sh.exec,'bower i')
+      ],function(err, result){
+        err = hasNoBowerJson(err, result) ? null : err;
+        callback(err, result);
+      });
+    }
+  };
+}
+
+function hasNoBowerJson(err, result){
+  var array = [];
+  result.forEach(function(data){
+    if(data && data.length) array = data;
+  });
+  return (err === 1 &&
+          array.length > 1 &&
+          array[1].indexOf('No bower.json present') > -1);
+}

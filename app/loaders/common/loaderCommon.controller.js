b437e3f4b8f70fecb6bcf26f4a7713a7204fe826
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-04-26 13:43:21 +0200

    createTree undefined fix

--- app/loaders/common/loaderCommon.controller.js
+++ app/loaders/common/loaderCommon.controller.js
@@ -5,12 +5,13 @@
     .module('king.loaders.common')
     .controller('commonLoaderCtrl', commonLoaderCtrl);
 
-  commonLoaderCtrl.$inject = ['$scope', '$window', '$rootScope', '$route', '$location', 'structureService', 'angularLoader', 'trafficGuardiaCivil'];
+  commonLoaderCtrl.$inject = ['$scope', '$interval', '$rootScope', '$route', '$location', 'structureService', 'angularLoader', 'trafficGuardiaCivil'];
 
-  function commonLoaderCtrl($scope, $window, $rootScope, $route, $location, structureService, angularLoader, trafficGuardiaCivil) {
+  function commonLoaderCtrl($scope, $interval, $rootScope, $route, $location, structureService, angularLoader, trafficGuardiaCivil) {
     // console.log('[V] Pasa por el commonLoaderCtrl');
 
     var app = document.querySelector('#app');
+    var firstInterval = {};
 
     $rootScope.showTransition = true;
     $location.$$path = $location.$$path || '/';
@@ -52,7 +53,7 @@
             }
             //Render external calls generating new elements
             if (count === 0 && prev > 0) {
-              renderKoaApp();
+              firstInterval = $interval(renderKoaApp, 10);
             }
 
             prev = count;
@@ -252,15 +253,18 @@
     }
 
     function renderKoaApp() {
-      app.createTree(function() {
-        if (app.theme) {
-          renderElements()
-        } else if ($rootScope.appData) {
-          setTheme($rootScope.appData.config)
-        } else {
-          setTheme(structureService.getConfig());
-        }
-      });
+      if (app.createTree) {
+        $interval.cancel(firstInterval);
+        app.createTree(function() {
+          if (app.theme) {
+            renderElements()
+          } else if ($rootScope.appData) {
+            setTheme($rootScope.appData.config)
+          } else {
+            setTheme(structureService.getConfig());
+          }
+        });
+      }
     }
   }
 }());

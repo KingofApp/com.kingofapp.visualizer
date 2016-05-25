965074a4a48be07c676138df85062cdfcc8e100c
Abdón Rodríguez Davila <thepeer@kingofapp.es>
2016-05-25 13:11:45 +0200

    Use the new gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,6 @@
 'use strict';
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var del = require('del');
-var eslint = require('gulp-eslint');
-var path = require('path');
-var reload = browserSync.reload;
-var DIST = 'dist';
-
-var dist = function(subpath) {
-  return !subpath ? DIST : path.join(DIST, subpath);
-};
-
-// Watch files for changes & reload
-gulp.task('serve', function() {
-  browserSync({
-    port: 9001,
-    notify: true,
-    logPrefix: 'KOA',
-    snippetOptions: {
-      rule: {
-        match: '<span id="browser-sync-binding"></span>',
-        fn: function(snippet) {
-          return snippet;
-        }
-      }
-    },
-    server: {
-      baseDir: ['app'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
-  });
-
-  gulp.watch(['app/**/*.html', '!app/bower_components/**/*.html'], ['lint', reload]);
-  gulp.watch(['app/{core,loaders}/**/*.js'], ['lint', reload]);
-  gulp.watch(['app/core/structure.json'], reload);
-  gulp.watch(['app/scripts/**/*.js'], ['lint', reload]);
-  gulp.watch(['app/styles/**/*.css'], reload);
-  gulp.watch(['app/images/**/*'], reload);
-  gulp.watch(['app/{modules,themes}/**/*'], reload);
-});
-
-// Clean output directory
-gulp.task('clean', function() {
-  return del(['.tmp', dist()]);
-});
-
-// Lint Javascript
-gulp.task('lint', function() {
-  return gulp.src(['gulpfile.js', 'app/**/{*.js,*.html}', '!app/{modules,themes,services}/**/*'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+// Load custom tasks from the `gulp-tasks` directory
+require('./gulp-tasks/clean.js');
+require('./gulp-tasks/lint.js');
+require('./gulp-tasks/serve.js');

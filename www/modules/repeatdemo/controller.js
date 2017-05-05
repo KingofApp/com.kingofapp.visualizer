e997f148c771538792f1aea707fccfce73c2235f
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2017-05-05 13:34:51 +0200

    Eventlisteners delegated to directive + dynamic attributes to content

--- www/modules/repeatdemo/controller.js
+++ www/modules/repeatdemo/controller.js
@@ -17,11 +17,31 @@
       return {
         restrict: 'EA',
         scope: true,
+        transclude: false,
         link: function (scope, element, attrs) {
-          console.log("element", element[0]);
-          // Replace polymer variables
-          element[0].heading = attrs.headingb;
-          element[0].image = attrs.imageb;
+          
+          for (var attr in attrs) {
+            if (typeof attrs[attr] === 'string') angular.element(element).find('.'+ attr).html(attrs[attr]);
+          }
+
+          //TODO: Create dynamic polymer element assignation
+          element[0].heading = attrs['koaHeading'];
+          element[0].image = attrs['koaImage'];
+
+          element.on('click', '[ng-click]', ngClickWrapper);
+          function ngClickWrapper(e) {
+
+            // Clean function name
+            var functionName = $(this).attr('ng-click').replace(/(\(.*?\))/, '');
+
+            //Match params
+            // var paramsList = getParams($(this).attr('ng-click'), scope);
+            console.log("Click handled by directive");
+            scope[functionName](e);
+
+            // $scope.$apply();
+            e.stopPropagation();
+          }
         }
       };
     })
@@ -33,15 +53,29 @@
     //Register upper level modules
     structureService.registerModule($location, $scope, 'repeatdemo');
 
-    // Load sample remote data
-    $scope.sampleData = [];
-    $http.get('https://jsonplaceholder.typicode.com/photos').then(function (response) {
-      for (var i = 0; i < 12; i++) {
-        $scope.sampleData.push(response.data[i]);
-      }
-      console.log($scope.sampleData);
-    });
+    loadData();
+    $scope.doSomething = function(e) {
+      console.log("E",e);
+    }
+    $scope.remove = function(e) {
+      console.log("E", e);
+      // console.log("Parent", angular.element( e.currentTarget  ).parent().parent());
+     angular.element( e.currentTarget  ).parent().parent().remove();
+    }
+    $scope.update = function(e) {
+      loadData();
+    }
 
+    function loadData() {
+          // Load sample remote data
+        $scope.sampleData = [];
+        $http.get('https://jsonplaceholder.typicode.com/photos').then(function (response) {
+          for (var i = 0; i < 12; i++) {
+            $scope.sampleData.push(response.data[i]);
+          }
+          console.log($scope.sampleData);
+        });
+    }
   }
 
 }());

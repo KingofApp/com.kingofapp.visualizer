95736411ae6f55aea8cd878b83526e5fdb79005a
Darryl García <darrylgarcia96@gmail.com>
2022-03-10 11:34:47 +0100

    replace EM6 promice by an angular js

--- www/core/polymer.service.js
+++ www/core/polymer.service.js
@@ -1,35 +1,31 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('king.core.structureService')
     .factory('$polymer', $polymer);
 
-  $polymer.$inject = [];
+  $polymer.$inject = ['$q'];
 
-  function $polymer() {
+  function $polymer($q) {
     return {
       importHref: importHref,
-      updateStyles: async() => {
-                        while(typeof Polymer === 'undefined');
-                        await new Promise(resolve => setTimeout(resolve, 1000));
-                        Polymer.updateStyles();
-                    },
+      updateStyles: $q.when(Polymer).then(function(){ Polymer.updateStyles() }),
       setCustomStyle: setCustomStyle
-    };
+      };
 
-    function importHref(href, onload, onerror) {
+      function importHref(href, onload, onerror) {
       var l = document.createElement('link');
       l.rel = 'import';
       l.href = href;
       var self = this;
       if (onload) {
-        l.onload = function(e) {
+        l.onload = function (e) {
           return onload.call(self, e);
         };
       }
       if (onerror) {
-        l.onerror = function(e) {
+        l.onerror = function (e) {
           return onerror.call(self, e);
         };
       }
